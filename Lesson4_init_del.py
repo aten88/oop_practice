9a52061c03c18efcd93e704d6ac7f903c7aba48e
Алексей Тен <virtuoso_ten@mail.ru>
2024-05-08 17:46:47 +0300

    Отредактировал уроки

--- Lesson4_init_del.py
+++ Lesson4_init_del.py
@@ -2,7 +2,7 @@ class Point:
     color = 'red'
     circle = 2
 
-    def __init__(self, x, y) -> None:
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
@@ -12,12 +12,12 @@ class Point:
     def set_coords(self, x, y):
         self.x = x
         self.y = y
-        print('Вызов метода set_coords')
+        return self.x + self.y
 
     def get_coords(self):
         return self.x, self.y
 
 
-pt = Point(0, 0)
+pt = Point()
 
-c = 5 + pt.x
+print(pt.__dict__)
