68e2b6ce09d63ebd3ed43343120b0abc1ef4123a
Алексей Тен <virtuoso_ten@mail.ru>
2024-05-13 17:54:04 +0300

    Проработал 3 типа методов

--- Lesson6_@classmethod_@staticmethod.py
+++ Lesson6_@classmethod_@staticmethod.py
@@ -1,1 +1,25 @@
-class 
+class Vector:
+    MIN_COORD = 0
+    MAX_COORD = 100
+
+    @classmethod
+    def validate(cls, arg):
+        return cls.MIN_COORD <= arg <= cls.MAX_COORD
+
+    def __init__(self, x, y):
+        self.x = self.y = 0
+        if self.validate(x) and self.validate(y):
+            self.x = x
+            self.y = y
+
+    def get_coords(self):
+        return self.x, self.y
+
+    @staticmethod
+    def norm2(x, y):
+        return x*x + y*y
+
+
+v = Vector(1, 2)
+print(Vector.get_coords(v))
+print(Vector.validate(5))
